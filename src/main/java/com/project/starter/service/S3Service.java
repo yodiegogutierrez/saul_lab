b51db2dfb300b97bb47030c1452afebc97e6055c
GUTID097 <diego.z.gutierrez.-nd@disney.com>
2021-07-25 20:05:48 -0500

    Added S3 upload method with aws s3 client

--- src/main/java/com/project/starter/service/S3Service.java
+++ src/main/java/com/project/starter/service/S3Service.java
@@ -1,14 +1,37 @@
 package com.project.starter.service;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.project.starter.model.Message;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class S3Service {
 
+    private final static String PATH_RESOURCES = "";
+
+    @Value("${aws-s3-bucket-arn}")
+    private String bucketName;
+
+    private AmazonS3 s3Client;
+
+    public S3Service() {
+        this.s3Client = AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.US_WEST_2)
+                .build();
+    }
+
     public Message processMessage(Message message) {
+        String file = new SimpleDateFormat("yyyyMMddHHmm'.txt'").format(new Date());
         message.setCreated(Instant.now());
+        s3Client.putObject(bucketName, file, message.getNote());
         return message;
     }
 }
